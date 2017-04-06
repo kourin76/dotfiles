0a5420987f55ac662eb67ef9285902146d77b450
tvnc <thfjrmof@googlemail.com>
2017-04-06 18:34:30 +0900

    update deploy script to create link to dot files

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 git clone https://github.com/tvnc/dotfiles.git .
+
 sh ./deploy.sh
 
--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,42 @@ if ! [ -e "$DEIN_DIR" ]; then
   sh ./installer.sh "$DEIN_DIR"
 fi
 
+# create symbolic link only when dot file is not linked.
+#if [ -L ~/.vimrc ]; then
+#  echo ~/.vimrc is symbolic link.
+#else
+#  if [ -f ~/.vimrc ]; then
+#    echo '~/.vimrc is found. Rename it to .vimrc_ for backup'
+#    mv ~/.vimrc ~/.vimrc_
+#    ln -Fis "$PWD/.vimrc" ~
+#  fi
+#fi
+
+#if [ -L ~/.gitconfig ]; then
+#  echo ~/.gitconfig is symbolic link.
+#else
+#  if [ -f ~/.gitconfig ]; then
+#    echo '~/.gitconfig is found. Rename it to .gitconfig_ for backup'
+#    mv ~/.gitconfig ~/.gitconfig_
+#    ln -Fis "$PWD/.gitconfig" ~
+#  fi
+#fi
+
+for dotfile in .?*; do
+  case $dotfile in
+    ..)
+      continue;;
+    .git)
+      continue;;
+    *)
+      if [ -L ~/$dotfile ]; then
+        echo ~/$dotfile is symbolic link.
+      else
+        if [ -f ~/$dotfile ]; then
+          echo ~/$dotfile  is found, create backup.
+          mv ~/$dotfileg ~/dotfile + '_'
+        fi
+        ln -Fis "$PWD/$dotfile" ~
+     fi
+  esac
+done
